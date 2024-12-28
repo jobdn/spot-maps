73bf81764737cb4ac556d9ab6457d42f8fedbcb6
Dan Pisarev <jobdan.dev@gmail.com>
2024-12-29 00:44:11 +0300

    fix coords

--- src/app/data/base.ts
+++ src/app/data/base.ts
@@ -1,8 +1,8 @@
 import { Place } from "@/shared/types/place";
 
 export const places: Place[] = [
-  { latitude: 43.992169, longitude: 56.330286, label: "Титре", text: "Сосал?" },
-  { latitude: 44.017614, longitude: 56.316175, label: "ПМ", text: "Сосал?" },
+  { latitude: 56.330286, longitude: 43.992169, label: "Титре", text: "Сосал?" },
+  { latitude: 56.316175, longitude: 44.017614, label: "ПМ", text: "Сосал?" },
 ].map(({ longitude, latitude, label, text }, i) => ({
   id: `${i}`,
   label,
--- src/entities/map/model/constants.ts
+++ src/entities/map/model/constants.ts
@@ -2,5 +2,5 @@ import type { YMapLocationRequest } from "@yandex/ymaps3-types";
 
 export const LOCATION: YMapLocationRequest = {
   center: [44.006516, 56.326797], // starting position [lng, lat]
-  zoom: 2, // starting zoom
+  zoom: 8, // starting zoom
 };
